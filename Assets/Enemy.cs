da6fa06bd3f508537561a6e90782f77d35b5dc25
More3428 <andmoreno@csumb.edu>
2023-03-18 20:27:53 -0700

    TurretsE04

---
+++ Assets/Enemy.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0]; 
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+        
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex]; 
+    }
+    
+}
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+   public static Transform[] points;
+
+   private void Awake()
+   {
+      points = new Transform[transform.childCount];
+      for (int i = 0; i < points.Length; i++)
+      {
+         points[i] = transform.GetChild(i); 
+      }
+   }
+   
+   
+}
